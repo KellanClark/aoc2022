c00e61d21352c5067d16be734368cf0ca68c2c62
Kellan Clark <86537480+KellanClark@users.noreply.github.com>
2022-12-01 08:44:48 -0500

    Day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,14 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    //let answer1: u64 = std::fs::read_to_string("input").unwrap().split("\n\n").map(|elf| elf.lines().map(|x| x.parse::<u64>().unwrap()).sum()).max().unwrap();
+
+    let mut elfs: Vec<u64> = fs::read_to_string("input").unwrap().split("\n\n").map(|elf| elf.lines().map(|x| x.parse::<u64>().unwrap()).sum()).collect();
+    elfs.sort();
+    elfs.reverse();
+    let answer1: u64 = elfs[0];
+    let answer2: u64 = elfs[..3].into_iter().sum();
+
+    println!("Part 1: {}", answer1);
+    println!("Part 2: {}", answer2);
 }
