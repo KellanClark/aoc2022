0a1dcdcb5a8f0f4032ca59ea7837338ce5304a37
Kellan Clark <86537480+KellanClark@users.noreply.github.com>
2022-12-08 16:56:10 -0500

    Day 7

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+#[derive(Debug)]
 struct Directory {
     size: u64,
     files: HashMap<String, Box<Directory>>,
@@ -14,38 +15,62 @@ impl Directory {
     }
 }
 
-fn fill(current_directory: &mut Directory, mut commands: std::str::Split<'_, &str>) {
-    let command = match commands.next() {
-        None => return,
-        Some(x) => {println!("{x}"); x.split_once(' ').unwrap()},
-    };
-
-    if command.0 == "cd" {
-        if command.1 == ".." {
-            return;
-        } else {
-            fill(current_directory.files.entry(command.1.to_string()).or_insert(Box::new(Directory::new())), commands);
-        }
-    } else if command.0 == "ls" {
-        for line in command.1.lines().skip(1) {
-            let entry = line.split_once(' ').unwrap();
-            if entry.0 == "dir" {
-                current_directory.files.insert(entry.0.to_string(), Box::new(Directory::new()));
-            } else {
-                current_directory.size += entry.0.parse::<u64>().unwrap();
+fn fill(current_directory: &mut Directory, commands: &mut std::str::Split<'_, &str>) {
+    loop {
+        if let Some(x) = commands.next() {
+            let command = x.split_once(&[' ', '\n'][..]).unwrap();
+            if command.0 == "cd" {
+                if command.1 == "..\n" {
+                    return;
+                } else {
+                    fill(current_directory.files.entry(command.1.trim().to_string()).or_insert(Box::new(Directory::new())), commands);
+                }
+            } else if command.0 == "ls" {
+                for line in command.1.lines() {
+                    let entry = line.split_once(' ').unwrap();
+                    if entry.0 == "dir" {
+                        current_directory.files.insert(entry.1.trim().to_string(), Box::new(Directory::new()));
+                    } else {
+                        current_directory.size += entry.0.parse::<u64>().unwrap();
+                    }
+                }
             }
+        } else {
+            return
         }
-    } else {
-        return;
     }
+}
+
+fn get_dir_size(current_directory: &mut Directory, answer1: &mut u64, answer2: &mut u64, space_to_free: u64) -> u64 {
+    let mut total_size = current_directory.size;
+    for (_name, dir) in &mut current_directory.files {
+        total_size += get_dir_size(dir, answer1, answer2, space_to_free);
+    }
+    if total_size <= 100000 {
+        *answer1 += total_size;
+    }
+    if (total_size >= space_to_free) && (total_size < *answer2) {
+        *answer2 = total_size;
+    }
+    total_size
 }
 
 fn main() {
     let input = std::fs::read_to_string("input").unwrap();
-    println!("Hello, world! {:?}", input.split(' '));
 
     let mut file_tree: Directory = Directory::new();
-    fill(&mut file_tree, input.split("$ "));
+    let mut commands = input.split("$ ");
+    commands.next();
+    commands.next();
+    fill(&mut file_tree, &mut commands);
 
-    println!("Hello, world! {input}");
+    let mut answer1: u64 = 0;
+    let mut answer2: u64 = 0;
+    let used_space = get_dir_size(&mut file_tree, &mut answer1, &mut answer2, 0);
+    answer1 = 0;
+    answer2 = u64::MAX;
+    get_dir_size(&mut file_tree, &mut answer1, &mut answer2, used_space - 40000000);
+
+    println!("Part 1: {answer1}");
+    println!("Part 2: {answer2}");
 }
