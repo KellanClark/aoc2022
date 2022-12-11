5373581063c684e152dc21d60cf157c22b77ea67
Kellan Clark <86537480+KellanClark@users.noreply.github.com>
2022-12-11 17:44:48 -0500

    Day 9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -2,37 +2,44 @@ use std::collections::HashSet;
 
 fn main() {
     let input = std::fs::read_to_string("input").unwrap();
-    let mut visited1: HashSet<(i32, i32)> = HashSet::new();
-    let mut visited9: HashSet<(i32, i32)> = HashSet::new();
-
-    let mut segments = [(0, 0); 10];
-    visited1.insert((0, 0));
-    visited9.insert((0, 0));
+    let mut visited1: HashSet<(i32, i32)> = HashSet::from([(0, 0)]);
+    let mut visited9: HashSet<(i32, i32)> = HashSet::from([(0, 0)]);
+    let mut segments: [(i32, i32); 10] = [(0, 0); 10];
 
     for line in input.lines() {
         let parsed = line.split_once(' ').unwrap();
         let distance = parsed.1.parse::<i32>().unwrap();
-        match parsed.0 {
-            "U" => segments[0].1 += distance,
-            "D" => segments[0].1 -= distance,
-            "L" => segments[0].0 -= distance,
-            "R" => segments[0].0 += distance,
+        let mut offset: (i32, i32) = match parsed.0 {
+            "U" => (0, -distance),
+            "D" => (0, distance),
+            "L" => (-distance, 0),
+            "R" => (distance, 0),
             _ => panic!()
-        }
-        //println!("{:?}", head_pos);
-        for seg in 1..=9 {
-            while ((segments[seg - 1].0 - segments[seg].0).abs() > 1) || ((segments[seg - 1].1 - segments[seg].1).abs() > 1) {
-                if segments[seg - 1].0 != segments[seg].0 {
-                    segments[seg].0 += (segments[seg - 1].0 - segments[seg].0).signum();
-                }
-                if segments[seg - 1].1 != segments[seg].1 {
-                    segments[seg].1 += (segments[seg - 1].1 - segments[seg].1).signum();
-                }
+        };
 
-                if seg == 1 {
-                    visited1.insert(segments[seg]);
-                } else if seg == 9 {
-                    visited9.insert(segments[seg]);
+        while (offset.0 != 0) || (offset.1 != 0) {
+            if offset.0 != 0 {
+                segments[0].0 += offset.0.signum();
+                offset.0 -= offset.0.signum();
+            } else {
+                segments[0].1 += offset.1.signum();
+                offset.1 -= offset.1.signum();
+            }
+
+            for seg in 1..=9 {
+                while ((segments[seg - 1].0 - segments[seg].0).abs() > 1) || ((segments[seg - 1].1 - segments[seg].1).abs() > 1) {
+                    if segments[seg - 1].0 != segments[seg].0 {
+                        segments[seg].0 += (segments[seg - 1].0 - segments[seg].0).signum();
+                    }
+                    if segments[seg - 1].1 != segments[seg].1 {
+                        segments[seg].1 += (segments[seg - 1].1 - segments[seg].1).signum();
+                    }
+
+                    if seg == 1 {
+                        visited1.insert(segments[seg]);
+                    } else if seg == 9 {
+                        visited9.insert(segments[seg]);
+                    }
                 }
             }
         }
